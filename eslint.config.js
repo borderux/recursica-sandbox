f20704ff8e18d4ebec2d216ee7fd9e280f1ff02c
Matt Massey <matt@borderux.com>
2025-08-06 12:52:40 -0700

    Added prettier and mantine config

--- eslint.config.js
+++ eslint.config.js
@@ -7,9 +7,10 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import prettierConfig from 'eslint-config-prettier'
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'builds']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -17,6 +18,7 @@ export default tseslint.config([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettierConfig,
     ],
     languageOptions: {
       ecmaVersion: 2020,
