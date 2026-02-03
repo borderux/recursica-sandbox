e04dbc74a842164e80c5b7589e64857c3b05444e
mlmassey <mattlmassey@gmail.com>
2026-02-03 23:46:00 +0000

    Deploying to gh-pages from @ borderux/recursica-sandbox@104be477b26be6f3a8a25bc2527bab9b3398e29f 🚀

---
+++ storybook/assets/Hello.stories-Ba_i6cpq.js
@@ -0,0 +1,9 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";const o={title:"Example/Hello",parameters:{layout:"centered"}},e={render:()=>r.jsxs("div",{style:{padding:"20px",textAlign:"center"},children:[r.jsx("h1",{children:"Hello World! you suck new change again"}),r.jsx("p",{children:"Your PR preview is working! 🎉"})]})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  render: () => <div style={{
+    padding: '20px',
+    textAlign: 'center'
+  }}>
+      <h1>Hello World! you suck new change again</h1>
+      <p>Your PR preview is working! 🎉</p>
+    </div>
+}`,...e.parameters?.docs?.source}}};const s=["HelloWorld"];export{e as HelloWorld,s as __namedExportsOrder,o as default};
